217a29ad01c750a25e5d3d7179c690ca75b05cf2
rahulkumar828515 <rahul.kumar@sparsadigital.in>
2025-07-13 14:22:42 +0530

    Add blog footer for common use

---
+++ include/blogsFooter.php
@@ -0,0 +1,24 @@
+<div class="blogPage">
+            <h3 class="mt-5">Latest Blog</h3>
+            <a href="/blogs-and-works" class="blogBtn">View all</a>    
+        </div>
+        
+        <div class="row">
+          <div class="col-md-4 blogHoverEffect">
+            <div class="card blog-card border-0">
+              <img src="https://cdn.pixabay.com/photo/2024/03/08/07/31/ai-generated-8620060_1280.jpg" class="card-img-top" alt="Blog 1">
+              <div class="card-body">
+                <small class="text-muted">30 Jan 2024 • 10 mins read</small>
+                <h6 class="card-title mt-2">Unveiling the Secrets Beyond the Tourist Trails</h6>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-4 blogHoverEffect">
+            <div class="card blog-card border-0">
+              <img src="https://cdn.pixabay.com/photo/2024/03/08/07/31/interior-8620061_1280.jpg" class="card-img-top" alt="Blog 2">
+              <div class="card-body">
+                <small class="text-muted">5 Feb 2024 • 8 mins read</small>
+                <h6 class="card-title mt-2">How to Style Your Furniture Clean</h6>
+              </div>
+            </div>
+          </div>
